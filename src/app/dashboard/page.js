c1baee8c7e58de8bdd386077da28290e81ce395f
Prabandh Chandrakanth Naik <prabandhnaik4305@gmail.com>
2025-09-06 13:14:36 +0530

    Add login page and authentication logic

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,7 @@
 // app/dashboard/page.js
 'use client';
 import { useEffect, useState } from 'react';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '../../../lib/supabaseClient';
 import { useRouter } from 'next/navigation';
 
 export default function Dashboard() {
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,7 @@
 // app/login/page.js
 'use client'; // This is required for Next.js 13+ App Router
 import { useState } from 'react';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '../../../lib/supabaseClient';
 import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
